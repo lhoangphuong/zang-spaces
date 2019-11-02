36ab287d99ac2fdba4cbd2fa1d1a69c1fe0dca69
lhoangphuong <lehoangbro@gmail.com>
2019-11-02 10:18:13 +0700

    This is auto commit!

--- Tests/test_python_org_search.py
+++ Tests/test_python_org_search.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
 
     def test_search_in_python_org(self):
         driver = self.driver
