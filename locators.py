492205cd4992e0ac891580b701ae00b3c9cddca5
lhoangphuong <lehoangbro@gmail.com>
2019-11-04 00:24:03 +0700

    This is auto commit!

--- locators.py
+++ locators.py
@@ -2,15 +2,14 @@ from selenium.webdriver.common.by import By
 
 class Locators():
     # --- Account Page Locators ---
-    GOOGLE_SSO=(By.XPATH, '//*[@id="sso_providers"]/a[1]/i/img')
-    OFFICE365_SSO=(By.XPATH,'//*[@id="sso_providers"]/a[2]/i/img')
+    GOOGLE_SSO=(By.CSS_SELECTOR, 'a.sso_provider:nth-child(1) > i:nth-child(1) > img:nth-child(1)')
+    OFFICE365_SSO=(By.CSS_SELECTOR, 'a.sso_provider:nth-child(2)')
 
     # -- Google Sign In Locators ---
-    GOOGLE_ID_TEXTBOX=(By.ID, 'identifierId')
-    GOOGLE_NEXT_BUTTON_1=(By.CSS_SELECTOR, '#identifierNext > span:nth-child(3)')
+    GOOGLE_ID_TEXTBOX=(By.CSS_SELECTOR, '#identifierId')
+    GOOGLE_NEXT_BUTTON_1=(By.CSS_SELECTOR, '#identifierNext > span:nth-child(3) > span:nth-child(1)')
     GOOGLE_PASSWORD_TEXBOX=(By.NAME, 'password')
-    GOOGLE_NEXT_BUTTON_2=(By.CSS_SELECTOR, '#passwordNext > span:nth-child(3)')
-
+    GOOGLE_NEXT_BUTTON_2=(By.CSS_SELECTOR, '#passwordNext > span:nth-child(3) > span:nth-child(1)')
     # -- Office365 Sign In Locators ---
     OFFICE365_ID_TEXTBOX=(By.ID, 'i0116')
     OFFICE365_NEXT_BUTTON=(By.ID, 'idSIButton9')
